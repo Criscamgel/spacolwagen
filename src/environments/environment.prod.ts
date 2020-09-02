010c7827232cd36d1a57952226885d1b27e3a9b9
Camilo <camiloguio@gmail.com>
2020-09-02 11:53:37 -0500

    Correccion Puerto API

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  urlA: "https://api.premiercredit.co:11445/PremierServices_API_EXT/api/login/authenticate",
-  urlV: "https://api.premiercredit.co:11445/PremierServices_API_EXT/api/viabilizacion/getviabilizacion"
+  urlA: "https://api.premiercredit.co:11444/PremierServices_API_EXT/api/login/authenticate",
+  urlV: "https://api.premiercredit.co:11444/PremierServices_API_EXT/api/viabilizacion/getviabilizacion"
 };
