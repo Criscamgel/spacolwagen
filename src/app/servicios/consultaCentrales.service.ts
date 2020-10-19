74960179c65b590034f496f26b9216a0f705e72a
Camilo <camiloguio@gmail.com>
2020-10-19 17:02:22 -0500

    changes necesary

--- src/app/servicios/consultaCentrales.service.ts
+++ src/app/servicios/consultaCentrales.service.ts
@@ -30,7 +30,7 @@ export class ConsultaCentralesService {
       ValorFinanciar: null,
       UsuarioRadica: 'k.maldonadoo',
       ConcesionarioRadicacion: 99,
-      IdentificacionVendedor: 15,
+      IdentificacionVendedor: 121,
     },
     DatosVehiculo: {
       Marca: 22
